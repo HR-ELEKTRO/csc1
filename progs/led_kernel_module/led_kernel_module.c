7c865a1a3def12c682a6005cbd99e7889c8216ae
Harry Broeders <J.Z.M.Broeders@hr.nl>
2025-11-22 18:45:44 +0100

    Betere namen

--- progs/led_kernel_module/led_kernel_module.c
+++ progs/led_kernel_module/led_kernel_module.c
@@ -4,26 +4,26 @@
 #include <linux/io.h>
 
 static void *mem_ptr;
-static volatile unsigned char *LED_ptr;
+static volatile unsigned char *led_ptr;
 
-static int __init hello_init(void) {
+static int __init led_init(void) {
 	mem_ptr = ioremap(0xFF200010, 1);
 	if (mem_ptr == NULL) {
 		pr_err("ERROR: memory of device can not accessed\n");
 		return -ENOMEM;
 	}
-	LED_ptr = mem_ptr;
-	*LED_ptr |= 0x01; 
+	led_ptr = mem_ptr;
+	*led_ptr |= 0x01; 
 	return 0;
 }
 
-static void __exit hello_exit(void) {
-	*LED_ptr &= ~0x01;
+static void __exit led_exit(void) {
+	*led_ptr &= ~0x01;
 	iounmap (mem_ptr);
 }
 
-module_init(hello_init);
-module_exit(hello_exit);
+module_init(led_init);
+module_exit(led_exit);
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Harry Broeders");
