6ff3b71233d2cfa36c7ca3687c38fd95fb2a3d83
Harry Broeders <J.Z.M.Broeders@hr.nl>
2025-11-23 23:59:18 +0100

    Verbeteringen nadat de image is gemaakt: 1. pr_info en pr_err i.p.v. printk 2. writel i.p.v. pointer redirectie 3. major device number gelogd met pr_info 4. globale variabel dev gebruikt i.p.v. MKDEV(dev_major, 0) en globale variabele dev_major verwijderd.

--- home/student/CSC10_Development/led_driver/led_module.c
+++ home/student/CSC10_Development/led_driver/led_module.c
@@ -1,5 +1,6 @@
 #include <linux/cdev.h>
 #include <linux/fs.h>
+#include <linux/io.h>
 #include <linux/platform_device.h>
 #include <linux/of.h>
 
@@ -8,12 +9,10 @@
 // data can be global because there will be no minor devices
 
 // virtual address of leds PIO
-static volatile unsigned int *PIO_ptr;
+static void __iomem *pio_ptr;
 
 // device number
 static dev_t dev = 0;
-// device major number
-static int dev_major = 0;
 
 // device class structure
 static struct class *my_dev_class = NULL;
@@ -23,9 +22,9 @@ static struct cdev my_dev_cdev;
 
 static int my_dev_open(struct inode *inode, struct file *file) 
 {
-	printk(KERN_INFO DEVNAME ": Device open\n");
+	pr_info(DEVNAME ": Device open\n");
 	if ((file->f_flags & O_WRONLY) != O_WRONLY) {
-		printk(KERN_ALERT DEVNAME ": ERROR: Device opened not in write only mode\n");
+		pr_err(DEVNAME ": ERROR: Device opened not in write only mode\n");
 		return -EACCES;
 	} 
 	return 0;
@@ -33,7 +32,7 @@ static int my_dev_open(struct inode *inode, struct file *file)
 
 static int my_dev_release(struct inode *inode, struct file *file)
 {
-	printk(KERN_INFO DEVNAME ": Device close\n");
+	pr_info(DEVNAME ": Device close\n");
 	return 0;
 }
 
@@ -41,7 +40,7 @@ static ssize_t my_dev_write(struct file *file, const char __user *buf, size_t co
 {
 	char buffer[128];
 	size_t maxdatalen = sizeof buffer;
-	printk(KERN_INFO DEVNAME ": Device write\n");
+	pr_info(DEVNAME ": Device write\n");
 
 	if (count < maxdatalen) {
 		maxdatalen = count;
@@ -49,15 +48,15 @@ static ssize_t my_dev_write(struct file *file, const char __user *buf, size_t co
 
 	unsigned long ret = copy_from_user(buffer, buf, maxdatalen);
 	if (ret) {
-		printk(KERN_ALERT DEVNAME ": ERROR: copy_from_user failed\n");
+		pr_err(DEVNAME ": ERROR: copy_from_user failed\n");
 		return ret;
 	}
 
 	size_t i;
 	for (i = 0; i < maxdatalen; i++) {
 		int value = (int)buffer[i];
-		printk(KERN_INFO DEVNAME ": Data written 0x%02X\n", value);
-		*PIO_ptr = value;
+		pr_info(DEVNAME ": Data written 0x%02X\n", value);
+		writel(value, pio_ptr);
 	}
 
 	return maxdatalen;
@@ -76,7 +75,7 @@ static int my_dev_uevent(const struct device *dev, struct kobj_uevent_env *env)
 {
 	int ret = add_uevent_var(env, "DEVMODE=%#o", 0666);
 	if (ret < 0) {
-		printk(KERN_ALERT DEVNAME ": ERROR: add_uevent_var failed\n");
+		pr_err(DEVNAME ": ERROR: add_uevent_var failed\n");
 		return ret;
 	}
 	return 0;
@@ -84,25 +83,25 @@ static int my_dev_uevent(const struct device *dev, struct kobj_uevent_env *env)
 
 static int init_handler(struct platform_device *pdev)
 {
-	printk(KERN_INFO DEVNAME ": Create character device\n");
+	pr_info(DEVNAME ": Create character device\n");
 
 	// allocate a range of char device numbers
 	// in this case only one minor number
 	// the major number will be assigned dynamically
 	int err = alloc_chrdev_region(&dev, 0, 1, "leds");
 	if (err < 0) {
-		printk(KERN_ALERT DEVNAME ": ERROR: no major device number available\n");
+		pr_err(DEVNAME ": ERROR: no major device number available\n");
 		return err;
 	}
-	// extract the assigned major device number
-	dev_major = MAJOR(dev);
+	// log the assigned major device number
+	pr_info(DEVNAME ": Major device number is %d\n", MAJOR(dev));
 
 	// create device class
 	// class leds is a reserved class name and can not be used. Therefore we use my_leds
 	my_dev_class = class_create("my_leds");
 
 	if (IS_ERR(my_dev_class)) {
-		printk(KERN_ALERT DEVNAME ": ERROR: class can not be created for device\n");
+		pr_err(DEVNAME ": ERROR: class can not be created for device\n");
 		err = PTR_ERR(my_dev_class);
 		goto cleanup_chrdev_region;
 	}
@@ -114,32 +113,31 @@ static int init_handler(struct platform_device *pdev)
 	my_dev_cdev.owner = THIS_MODULE;
 
 	// create device in /sys/devices/virtual/my_leds/leds
-	err = cdev_add(&my_dev_cdev, MKDEV(dev_major, 0), 1);
+	err = cdev_add(&my_dev_cdev, dev, 1);
 	if (err < 0) {
-		printk(KERN_ALERT DEVNAME ": ERROR: device number can not be added for device\n");
+		pr_err(DEVNAME ": ERROR: device number can not be added for device\n");
 		goto cleanup_class;
 	}
 
 	// create device node /dev/leds
-	struct device *my_dev = device_create(my_dev_class, NULL, MKDEV(dev_major, 0), NULL, "leds");
+	struct device *my_dev = device_create(my_dev_class, NULL, dev, NULL, "leds");
 	if (IS_ERR(my_dev)) {
-		printk(KERN_ALERT DEVNAME ": ERROR: device can not be created for driver\n");
+		pr_err(DEVNAME ": ERROR: device can not be created for driver\n");
 		err = PTR_ERR(my_dev);
 		goto cleanup_cdev;
 	}
 
 	// map the PIO device registers into virtual memory
-	void *mem_ptr = devm_platform_ioremap_resource(pdev, 0);
-	if (IS_ERR(mem_ptr)) {
-		printk(KERN_ALERT DEVNAME ": ERROR: no base address found for PIO device\n");
-		err = PTR_ERR(mem_ptr);
+	pio_ptr = devm_platform_ioremap_resource(pdev, 0);
+	if (IS_ERR(pio_ptr)) {
+		pr_err(DEVNAME ": ERROR: no base address found for PIO device\n");
+		err = PTR_ERR(pio_ptr);
 		goto cleanup_device;
 	}
-	PIO_ptr = mem_ptr;
 	return 0;
 	// cleanup on errors
 cleanup_device:	
-	device_destroy(my_dev_class, MKDEV(dev_major, 0));
+	device_destroy(my_dev_class, dev);
 cleanup_cdev:
 	cdev_del(&my_dev_cdev);
 cleanup_class:
@@ -151,9 +149,9 @@ cleanup_chrdev_region:
 
 static void clean_handler(struct platform_device *pdev)
 {
-	printk(KERN_INFO DEVNAME ": Destroy character device\n");
+	pr_info(DEVNAME ": Destroy character device\n");
 
-	device_destroy(my_dev_class, MKDEV(dev_major, 0));
+	device_destroy(my_dev_class, dev);
 	cdev_del(&my_dev_cdev);
 	class_destroy(my_dev_class);
 	unregister_chrdev_region(dev, 1);
@@ -161,7 +159,7 @@ static void clean_handler(struct platform_device *pdev)
 
 // describe which device we want to bind to this kernel module
 // this must match an entry in the device tree
-static const struct of_device_id mijn_module_id[] ={
+static const struct of_device_id mijn_module_id[] = {
 	{.compatible = "leds"},
 	{}
 };
